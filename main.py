b56c26c36116e8b8d8120822d7540cc7225894d8
Nathalia <nathanastacio@gmail.com>
2024-10-03 10:46:16 -0300

    Novo endpoint

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+import requests
 
 app = FastAPI()
 
 @app.get('/api/hello')
 def hello_world():
+    '''
+    Endpoint que exibe uma mensagem famosa do mundo da programação.
+    '''
     return {'Hello':'World'}
+
+@app.get('/api/restaurantes/')
+def get_restaurantes(restaurante: str = Query(None)):
+    '''
+    Endpoint para ver cardápios dos restaurantes.
+    '''
+    url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        dados_json = response.json()
+        if restaurante is None:
+            return {'Dados': dados_json}
+        
+        dados_do_restaurante = []
+        for item in dados_json:
+            if item['Company'] == restaurante:
+                dados_do_restaurante.append({
+                    'item': item['Item'],
+                    'price': item['price'],
+                    'description': item['description']
+                })
+        return {'Restaurante': restaurante, 'Cardapio': dados_json}
+    else:
+        return {'Erro': f'{response.status_code} - {response.text}'}
